8b9eb1a55e781718412f686131e0ebe46b1dbb82
DESKTOP-DADL9LN\Alumno 3 <jtapia@cocoschool.mx>
2022-09-21 18:00:12 -0500

    Se agrego codigo de CPP y variables en BP

---
+++ Source/Proyecto_Mamalon_Luigi/MyCharacterLuigi.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCharacterLuigi.h"
+
+// Sets default values
+AMyCharacterLuigi::AMyCharacterLuigi()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyCharacterLuigi::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyCharacterLuigi::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+void AMyCharacterLuigi::Dummy()
+{
+
+}
+// Called to bind functionality to input
+void AMyCharacterLuigi::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Proyecto_Mamalon_Luigi/MyCharacterLuigi.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacterLuigi.generated.h"
+
+UCLASS()
+class PROYECTO_MAMALON_API AMyCharacterLuigi : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMyCharacterLuigi();
+	UPROPERTY(Category = Character, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	int life;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void Dummy();
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
